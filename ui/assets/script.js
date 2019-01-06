d7e4d6956bb3abe9bce670485531827799f0a0ef
HarrietAkot <44574124+HarrietAkot@users.noreply.github.com>
2019-01-06 19:37:17 +0300

    arefactor the ui code

---
+++ ui/assets/script.js
@@ -0,0 +1,8 @@
+function validate(form){
+    var returnValue = true;
+    var username = form.username.value;
+    var email = form.email.value;
+    var password = form.password.value;
+
+}
+document.alert('hello harriet')
